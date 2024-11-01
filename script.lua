335eae9a1913d211b48e2aa3e8d29d36f64379cb
unknownsourcelab <yunoy779@gmail.com>
2024-11-01 12:23:56 +0800

    script.lua

--- script.lua
+++ script.lua
@@ -25,6 +25,7 @@ local Window = Rayfield:CreateWindow({
        Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
  })
+
  local MainTab = Window:CreateTab("Home", nil) -- Title, Image
  local MainSection = MainTab:CreateSection("Modes")
 
